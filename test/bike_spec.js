c7d2235c3caa892d196249830a130e0423c1f928
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-11 23:53:33 +0100

    Adds beforeEach to DRY up code

--- test/bike_spec.js
+++ test/bike_spec.js
@@ -1,16 +1,19 @@
 
 describe('Bike', function() {
-  
+
   var bike;
-   
+    
+  beforeEach(function() {
+    bike = new Bike()
+  });
+
+  
     describe('working', function() {
       it('by default the bike works', function() {
-        bike = new Bike();
-        expect(bike.works).toEqual(true);
+      expect(bike.works).toEqual(true);
       })
 
       it('can also break', function() {
-        bike = new Bike();
         bike.break()
         expect(bike.works).toEqual(false);
       });
@@ -19,13 +22,11 @@ describe('Bike', function() {
 
     describe('report_broken', function() {
       it('can be reported as broken if it is broken', function() {
-        bike = new Bike()
         bike.break()
         expect(bike.report_broken()).toEqual("Bike has been reported as broken")
       });
 
       it('cannot be report as broken if it is not broken', function() {
-        bike = new Bike()
         expect(function() { bike.report_broken(); }).toThrowError('Unable to report as broken. Bike is not broken');
       });
     });
